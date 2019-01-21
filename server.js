a61a620e5a5ebd2591e25b5b8d5857d5ac8d1d6c
onsattia <onesattia@gmail.com>
2019-01-21 19:09:24 +0100

    add body-parser

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const routes = require("./routes/students");
 // set up express app
 const app = express();
 
-// body-parser middleware
-app.use(bodyParser.json());
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 // init routes
 app.use(routes);
