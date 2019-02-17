7e1796ad7657103dc05dd8b93d1301e6fd768521
onsattia <onesattia@gmail.com>
2019-02-17 07:34:50 +0100

    added login & register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
-import Admin from "./layouts/Admin";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./store/store";
+
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
